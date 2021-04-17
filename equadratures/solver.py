0efa092ad9b61cb243613b14c162a3bffa397f20
Simardeep27 <42802893+Simardeep27@users.noreply.github.com>
2021-04-17 17:59:47 +0530

    Add files via upload

--- equadratures/solver.py
+++ equadratures/solver.py
@@ -1016,8 +1016,10 @@ class elastic_net(Solver):
         Option to get the figure axes,figure.
 
     """
-        print(self.elements)    
-        return plot.plot_regpath(self,self.elements,nplot,save,show,return_figure)
+        if hasattr(self, self.elements):    
+            return plot.plot_regpath(self,self.elements,nplot,save,show,return_figure)
+        else:
+            return plot.plot_regpath(self,nplot,save,show,return_figure)
         
 # Custom solver subclass.
 #########################
